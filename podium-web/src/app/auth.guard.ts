4b297dcd7c294498a57c5b6927259bf83a17c5f4
Nicolas Seay <nicolas.seay@gmail.com>
2026-09-04 15:20:33 -0400

    Reverting some security changes

--- podium-web/src/app/auth.guard.ts
+++ podium-web/src/app/auth.guard.ts
@@ -1,12 +1,9 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { map } from 'rxjs';
 import { AuthService } from './auth.service';
 
 export const authGuard: CanActivateFn = () => {
   const auth = inject(AuthService);
   const router = inject(Router);
-  return auth
-    .ensureAuthenticated()
-    .pipe(map((authenticated) => authenticated || router.createUrlTree(['/login'])));
+  return auth.isAuthenticated() || router.createUrlTree(['/login']);
 };
